881deaed2cb9c80bf213f964765146615d00395f
LaneyS05 <laney.staggs05@gmail.com>
2023-12-04 16:10:51 -0500

    adding background img and store list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 import TopBar from "./TopBar";
+import Background from "./Background";
+import StoreListings from "./StoreListings";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <TopBar />
+      <Background />
+      <StoreListings />
+    </div>
+  );
 }
 export default App;
--- src/TopBar.js
+++ src/TopBar.js
@@ -1,22 +1,60 @@
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
 
-function TopBar() {
+export default function TopBar() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
-        <img src="logo192.png" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto"></Nav>
-        <Nav>
-          <Nav.Link href="#LogIn">Log In</Nav.Link>
-          <Button variant="success">Sign Up</Button>{" "}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
+          <img
+            style={{ position: "relative", right: 650 }}
+            src="https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg"
+            alt="logo"
+          />{" "}
+        </Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav
+            style={{
+              padding: "24px 40px 20px 40px",
+              fontSize: "18px",
+              lineHeight: "26px",
+              fontWeight: "bolder",
+              display: "flex",
+            }}
+          >
+            <Nav.Link
+              style={{
+                position: "relative",
+                left: 1250,
+                color: "#343538",
+                padding: 5,
+                fontSize: "18px",
+                top: -50,
+              }}
+              href="#LogIn"
+            >
+              Log In
+            </Nav.Link>
+            <Button
+              variant="success"
+              style={{
+                background: "lightgreen",
+                position: "relative",
+                left: 1260,
+                fontSize: "18px",
+                padding: 5,
+                margin: 10,
+                borderRadius: 20,
+                top: -55,
+              }}
+            >
+              Sign Up
+            </Button>{" "}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   );
 }
-
-export default TopBar;
